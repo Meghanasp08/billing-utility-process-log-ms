0b31d6332114f75a2defb6d0b8f6611329d2c950
firozekhan <firos.khan@activelobby.net>
2025-05-21 12:30:47 +0530

    update for api data competed

---
+++ src/configuration/enum/api-data.enum.ts
@@ -0,0 +1,16 @@
+export enum KeyName {
+    SETUP = 'setup',
+    DATA = 'data',
+    BALANCE = 'balance',
+    PAYMENT_BULK = 'payment-bulk',
+    PAYMENT_NON_BULK = 'payment-non-bulk',
+    CONFIRMATION = 'confirmation',
+    INSURANCE = 'insurance',
+}
+
+export enum ApiCategory {
+    SETUP_AND_CONSENT = 'Setup and Consent',
+    BANK_DATA_SHARING = 'Bank Data Sharing',
+    BANK_SERVICE_INITIATION = 'Bank Service Initiation',
+    INSURANCE = 'insurance',
+}
